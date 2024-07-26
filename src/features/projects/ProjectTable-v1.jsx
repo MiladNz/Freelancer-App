dfa1c13c3c4585e6d1c1d2c78f75323dd9759337
miladn <milad.n91@gmail.com>
2024-07-27 02:05:02 +0330

    compound component table

---
+++ src/features/projects/ProjectTable-v1.jsx
@@ -0,0 +1,38 @@
+import Empty from "../../ui/Empty";
+import Loading from "../../ui/Loading";
+import Table from "../../ui/Table";
+import toLocalDateShort from "../../utils/toLocalDateShort";
+import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
+import truncateText from "../../utils/truncateText";
+import ProjectRow from "./ProjectRow";
+import useOwnerPoject from "./useOwnerProjects";
+
+function ProjectTable() {
+  const { isLoading, projects } = useOwnerPoject();
+
+  if (isLoading) return <Loading />;
+  if (!projects.length) return <Empty resourceName="پروژه" />;
+
+  return (
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>عنوان پروژه</th>
+        <th>دسته بندی</th>
+        <th>بودجه</th>
+        <th>ددلاین</th>
+        <th>تگ ها</th>
+        <th>فریلنسر</th>
+        <th>وضعیت</th>
+        <th>عملیات</th>
+      </Table.Header>
+      <Table.Body>
+        {projects.map((project, index) => (
+          <ProjectRow key={project._id} project={project} index={index} />
+        ))}
+      </Table.Body>
+    </Table>
+  );
+}
+
+export default ProjectTable;
